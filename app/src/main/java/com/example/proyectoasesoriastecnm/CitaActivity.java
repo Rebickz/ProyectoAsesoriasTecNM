458655b2d27d69da7606b8ebab791b847f939c85
JoseloCordoba199 <jose.cordoba0192@gmail.com>
2021-06-10 03:32:49 -0500

    Se actualiza MateriasActivity con informacion de Profesor y se envia informacion al siguiente activity

--- app/src/main/java/com/example/proyectoasesoriastecnm/CitaActivity.java
+++ app/src/main/java/com/example/proyectoasesoriastecnm/CitaActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CitaActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class CitaActivity extends AppCompatActivity {
     private EditText tv1;
     private EditText materiA;
     private EditText EditHora;
+    private EditText Profesor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +37,56 @@ public class CitaActivity extends AppCompatActivity {
         String horaAgendada = getIntent().getStringExtra("horaAgendada");
         EditHora.setText(horaAgendada);
 
+        Profesor = (EditText) findViewById(R.id.EditTextProfesor);
+        String profesor = getIntent().getStringExtra("profesor");
+        Profesor.setText(profesor);
+
+    }
+
+
+    public void ClickMenu(View view){//Agregar
+        //Abrir drawer
+        MenuActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view){//Agregar
+        //Cerrar drawer
+        MenuActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view){//Agregar
+        //Redireccionar al Home
+        MenuActivity.redirectActivity(this,MenuActivity.class);
+    }
+
+    public void ClickPerfil(View view){//Agregar
+        //Recrear actividad
+        recreate();
+    }
+
+    public void ClickAgendar(View view){//Agregar
+        //Redireccionar
+        //redirectActivity(this, );
+        Toast.makeText(CitaActivity.this, "Boton Agendar", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void ClickCitas(View view){//Agregar
+        //Redireccionar
+        //redirectActivity(this, );
+        Toast.makeText(CitaActivity.this, "Boton Citas", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void ClickLogout(View view){//Agregar
+        //Cerrar app
+        MenuActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause() {//Agregar
+        super.onPause();
+        //Cerrar drawer
+        MenuActivity.closeDrawer(drawerLayout);
     }
 }
