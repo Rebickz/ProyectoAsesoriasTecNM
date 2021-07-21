9dde0334eae02995771319836975d96c3655ee82
Rebickz <zenberfco@outlook.com>
2021-07-20 21:19:41 -0500

    Proceso olvide contraseña

--- app/src/main/java/com/example/proyectoasesoriastecnm/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/proyectoasesoriastecnm/ForgotPasswordActivity.java
@@ -6,26 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class ForgotPasswordActivity extends AppCompatActivity  implements View.OnClickListener{
 
-    private Button signIn;
+    private EditText emailEditText;
+    private Button resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        signIn = (Button) findViewById(R.id.signIn);
-        signIn.setOnClickListener(this);
+        emailEditText = (EditText) findViewById(R.id.email);
+        resetPassword = (Button) findViewById(R.id.resetPassword);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.signIn:
-                startActivity(new Intent(this,MainActivity.class));
-                Toast.makeText(ForgotPasswordActivity.this, "Sent", Toast.LENGTH_LONG).show();
+            case R.id.resetPassword:
+                //startActivity(new Intent(this,MainActivity.class));
+                Toast.makeText(ForgotPasswordActivity.this, "Sent, verify your email", Toast.LENGTH_LONG).show();
                 break;
         }
     }
