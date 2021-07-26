beddaec10d2b2b6063ca74952a6374a6434aa69c
Rebickz <zenberfco@outlook.com>
2021-07-25 23:45:19 -0500

    Lesly: Profesor detalle cita y profesor citas funcionando

--- app/src/main/java/com/example/proyectoasesoriastecnm/ProfesorCitasActivity.java
+++ app/src/main/java/com/example/proyectoasesoriastecnm/ProfesorCitasActivity.java
@@ -6,11 +6,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,9 +26,10 @@ import com.google.firebase.database.ValueEventListener;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProfesorCitasActivity extends AppCompatActivity {
-    DrawerLayout drawerLayoutP;
+    /*DrawerLayout drawerLayoutP;
     ListView myListView;
     ArrayList<String> myArrayList;
     ArrayAdapter<String> adapter;
@@ -68,6 +74,98 @@ public class ProfesorCitasActivity extends AppCompatActivity {
         });
 
 
+    }*/
+
+    DrawerLayout drawerLayoutP;
+    ListView myListView;
+    ArrayList<Cita> myArrayList;
+    ArrayAdapter<Cita> adapter;
+    FirebaseDatabase database;
+    DatabaseReference mref;
+    Cita materia;
+    Cita materiaSelected;
+
+    private FirebaseUser user;
+
+
+    private List<Cita> listMateria = new ArrayList<Cita>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profesor_citas);
+
+        //SE AGREGA SHIMMER
+        /*ShimmerFrameLayout container =
+                (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.startShimmer(); // If auto-start is set to false
+        */
+
+
+        drawerLayoutP = findViewById(R.id.profesor_drawer_layout);
+
+        materia = new Cita();
+        myListView = (ListView) findViewById(R.id.ListViewMaterias);
+        database = FirebaseDatabase.getInstance();
+        mref = database.getReference("tablaCitas");
+
+        myArrayList = new ArrayList<Cita>();
+        //adapter = new ArrayAdapter<String>(this, R.layout.materia_info,R.id.nombre_materia, myArrayList);
+        adapter = new ArrayAdapter<Cita>(ProfesorCitasActivity.this, android.R.layout.simple_list_item_1, myArrayList);
+        mref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                for(DataSnapshot ds: snapshot.getChildren())
+                {
+                    materia = ds.getValue(Cita.class);
+                    //myArrayList.add(materia.getNombre() + "\n" + materia.getHorario() + "\n" + materia.getProfesor()+ "\n"+ materia.getDepartamento()+ "\n"+ materia.getLugar());
+                    myArrayList.add(materia);
+                }
+
+                /*final Handler handler = new Handler(Looper.getMainLooper());
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Do something after 100ms
+                        container.hideShimmer();
+                    }
+                }, 500);*/
+                myListView.setAdapter(adapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        //***********************INICIA ON CLICK LISTENER//***********************
+
+
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                Intent intent = new Intent(ProfesorCitasActivity.this, ProfesorDetalleCitaActivity.class);
+                materiaSelected = (Cita) adapterView.getItemAtPosition(i);
+                intent.putExtra("carrera", myListView.getItemAtPosition(i).toString());
+                intent.putExtra("email", user.getEmail());
+                intent.putExtra("fecha", materiaSelected.getFecha());
+                intent.putExtra("horaAgendada", materiaSelected.getHorario().toString());
+                intent.putExtra("lugar", materiaSelected.getLugar());
+                intent.putExtra("materia", materiaSelected.getMateria() );
+                intent.putExtra("profesor", materiaSelected.getProfesor().toString());
+                intent.putExtra("semestre", materiaSelected.getSemestre());
+                intent.putExtra("status", materiaSelected.getStatus());
+                intent.putExtra("uid", materiaSelected.getUid());
+                startActivity(intent);
+
+            }
+        });
     }
 
     public void ClickMenuP(View view){
@@ -120,10 +218,5 @@ public class ProfesorCitasActivity extends AppCompatActivity {
         super.onPause();
         //Cerramos drawer
         ProfesorMenuActivity.closeDrawer(drawerLayoutP);
-    }
-
-    public void ClickProfesorNombre_Cita(View view){
-        Intent i = new Intent( this, ProfesorDetalleCitaActivity.class);
-        startActivity(i);
     }
 }
--- app/src/main/java/com/example/proyectoasesoriastecnm/ProfesorDetalleCitaActivity.java
+++ app/src/main/java/com/example/proyectoasesoriastecnm/ProfesorDetalleCitaActivity.java
@@ -5,15 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import net.glxn.qrgen.android.QRCode;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfesorDetalleCitaActivity extends AppCompatActivity {
 
+    TextView datos;
+    String carrera;
+    String email;
+    String fecha;
+    String horaAgendada;
+    String lugar;
+    String materia;
+    String profesor;
+    String semestre;
+    String status;
+    String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profesor_detalle_cita);
-        String texto = "Datos cita\nMateria:Fundamentos de programación\nCarrera:TICS\nDepartamento:Sistemas y computacion\nHorario:12:00-13:00hrs\nLugar:Aula 45\n\n\nSesion registrada\nESTATUS DE LA SESION: COMPLETADA\n\n";
+
+        datos = (TextView) findViewById(R.id.datos_Cita);
+
+
+        carrera = getIntent().getStringExtra("carrera");
+        email = getIntent().getStringExtra("email");
+        fecha = getIntent().getStringExtra("fecha");
+        horaAgendada = getIntent().getStringExtra("horaAgendada");
+        lugar = getIntent().getStringExtra("lugar");
+        materia = getIntent().getStringExtra("materia");
+        profesor = getIntent().getStringExtra("profesor");
+        semestre = getIntent().getStringExtra("semestre");
+        status = getIntent().getStringExtra("status");
+        uid = getIntent().getStringExtra("uid");
+        datos.setText("Datos de la cita:" +"\nMateria: "+materia  +"\nLugar: "+ lugar +"\nProfesor: "+ profesor +"\nStatus: COMPLETADA");
+        String texto = "Datos de la cita:" +"\nMateria: "+materia  +"\nLugar: "+ lugar +"\nProfesor: "+ profesor +"\nStatus: COMPLETADA";
         Bitmap bitmap = QRCode.from(texto).bitmap();
 
         ImageView imagenCodigo = findViewById(R.id.qr);
