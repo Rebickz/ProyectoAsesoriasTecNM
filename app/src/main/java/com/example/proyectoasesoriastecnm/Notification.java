b07ff194ded4acca38f231974cd401a64d85e1db
davidAlejandroGonzalez <davidalejandrogonzalezbarba@gmail.com>
2021-07-27 00:27:20 -0500

    Ya se reciben notificaciones por medio de Firebase Cloud Messaging agregando nuevos metodos que crean la notificación con remoteMessage.
    Se modificó el gradle.build  ya que la aplicación alcanzó el limite de métodos permitidos 65K+. En AndroidManifest.xml se agregó codigo para que la clase Notification.java se ejecute sin necesidad de instanciarla, ya que esta estará recibiendo las notificaciones en tiempo real.

--- app/src/main/java/com/example/proyectoasesoriastecnm/Notification.java
+++ app/src/main/java/com/example/proyectoasesoriastecnm/Notification.java
@@ -1,71 +1,50 @@
 package com.example.proyectoasesoriastecnm;
 
-import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
-import androidx.annotation.NonNull;
+
 import androidx.core.app.NotificationCompat;
-
-
-
-
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
 
-import org.jetbrains.annotations.NotNull;
+
 
 import java.util.Random;
 
 
 public class Notification extends FirebaseMessagingService {
-/*
     @Override
-    public void onNewToken(String tokenUsuario) {
-        super.onNewToken(tokenUsuario);
-
+    public void onNewToken(String s) {
+        super.onNewToken(s);
     }
-
-    private void saveToken(String token){
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("tablaUsuarios").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-        ref.child("token").setValue(token);
-    }
-*/
     @Override
-    public void onMessageReceived(@NonNull @NotNull RemoteMessage remoteMessage) {
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
         super.onMessageReceived(remoteMessage);
 
         if (remoteMessage.getData().size() > 0){
-
             String titulo = remoteMessage.getData().get("titulo");
             String detalle = remoteMessage.getData().get("detalle");
-
-            afterOreo(titulo,detalle);
-
-
-
+                crearNotificacion(titulo,detalle);
 
         }
 
-
-
-
     }
 
-    public  void  afterOreo(String titulo, String detalle){
+    public  void crearNotificacion(String titulo, String detalle){
 
         String id = "mensaje";
 
         NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationCompat.Builder constructorNotification = new NotificationCompat.Builder(this,id);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel nc = new NotificationChannel(id,"nueva", NotificationManager.IMPORTANCE_HIGH);
             nc.setShowBadge(true);
             assert  nm!= null;
@@ -90,4 +69,5 @@ public class Notification extends FirebaseMessagingService {
         intentNoti.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         return PendingIntent.getActivity(this,0,intentNoti,0);
     }
+
 }
